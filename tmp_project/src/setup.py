5e561b306f073d9fbd0b0fd62d52821d4c312e5e
Anatoly <antauren@yandex.ru>
2021-03-10 16:27:21 +0300

    Update setup.py

--- tmp_project/src/setup.py
+++ tmp_project/src/setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='demo2',
+    name='demo3',
     version='0.3.1',
     description='News for Django',
     author='Elias',
     # namespace_packages=['home'],  # line 8
     packages=find_packages(),
     platforms='any',
-    #zip_safe=False,
+    # zip_safe=False,
     include_package_data=True,
     # dependency_links=['git+ssh://git@git.home.com/app-admintools@v0.1#egg=admintools-0.1'],  # line 13
-    # install_requires=['admintools==0.1'],  # line 14
+    install_requires=['requests==2.24.0'],  # line 14
     # classifiers=[
     #     'Development Status :: 5 - Production/Stable',
     #     'Environment :: Web Environment',
